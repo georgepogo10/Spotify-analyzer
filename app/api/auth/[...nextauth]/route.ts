274395ac4bdb1c864998d4b1dd9aabc86fc42fd6
georgepogo10 <83048586+georgepogo10@users.noreply.github.com>
2025-06-24 21:39:09 -0400

    minor updates to prepare for deployment

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,26 +1,24 @@
 // app/api/auth/[...nextauth]/route.ts
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 
-const authOptions = {
+const authOptions: NextAuthOptions = {
   providers: [
     SpotifyProvider({
       clientId: process.env.SPOTIFY_CLIENT_ID!,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
       authorization: {
-        params: {
-          scope: "user-top-read user-read-recently-played",
-        },
+        params: { scope: "user-top-read user-read-recently-played" },
       },
     }),
   ],
   callbacks: {
+    // TS will infer { token, account } types here
     async jwt({ token, account }) {
-      if (account?.access_token) {
-        token.accessToken = account.access_token;
-      }
+      if (account?.access_token) token.accessToken = account.access_token;
       return token;
     },
+    // TS will infer { session, token } types here
     async session({ session, token }) {
       session.user.accessToken = token.accessToken;
       return session;
@@ -30,5 +28,4 @@ const authOptions = {
 };
 
 const handler = NextAuth(authOptions);
-
 export { handler as GET, handler as POST };
